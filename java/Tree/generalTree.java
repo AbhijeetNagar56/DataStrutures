441ce044aaa271f04f2ce4e9f1b8769fee7c52e3
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-02-07 23:15:02 +0530

    adding java files for graph

--- java/Tree/generalTree.java
+++ java/Tree/generalTree.java
@@ -1,9 +1,11 @@
+import java.util.Queue;
+import java.util.LinkedList;
 
-class node {
+class Node {
     int val;
-    node left;
-    node right;
-    node(int data) {
+    Node right;
+    Node left;
+    Node(int data) {
         this.val = data;
         this.left = this.right = null; 
     }
@@ -11,79 +13,82 @@ class node {
 
 // binary tree
 class tree {
-    node root;
-    tree (int rootVal) {
-        this.root =  new node(rootVal);
-    }
+    Node root;
 
     // adding an element assuming root is not null
-    public void insert(node root, int key) {
-        if(root.left == null) {
-            root.left = new node(key);
-        } else if (root.right == null){
-            root.right = new node(key);
-        } else {
-            if (root.left.left == null || root.left.right == null) {
-                insert(root.left, key);
-            } else {
-                insert(root.right, key);
-            }
+    void insert(Node root, int key) {
+        Node newNode = new Node(key);
+        if (root == null) {
+            root = newNode;
+            return;
         }
-        System.out.println(key + " inserted successfully");
+    
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node curr = q.poll();
+
+            if (curr.left == null) {
+                curr.left = newNode;
+                return;
+            } else {q.add(curr.left);}
+            if (curr.right == null) {
+                curr.right = newNode;
+                return;
+            } else {q.add(curr.right);}
+        }
+        
     }
 
     // deleting an element
-    public void delete(node root, int key) {
-        if(root.val == key) {
-            if (root.left == null && root.right == null) {
-                root = null;
-            } else if (root.left == null || root.right == null) {
-                root = root.left != null ? root.left : root.right;
-            } else {
-                node curr = root;
-                while(curr.left != null) {
-                    curr = curr.left;
-                }
-                root.val = curr.val;
-                delete(curr, key);
-            }
-        } else {
-            delete(root.left, key);
-            delete(root.right, key);
-        }
+    void delete(Node root, int key) {
+        
     }
 
     // DFS traversal 
-    public void inorder(node root) {
+    void inOrder(Node root) {
         if (root == null) return;
-        inorder(root.left);
+        inOrder(root.left);
         System.out.print(root.val + " ");
-        inorder(root.right);
+        inOrder(root.right);
     }
 
+    void preOrder(Node root) {
+        if (root == null) return;
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    void postOrder(Node root) {
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+    
+
     // BFS traversal
-    public void levelOrder(node root) {
-        node que[] = new node[100];
-        int front = -1, rear = -1; 
-        que[++rear] = root;
-        while (rear > front) {
-            node curr = que[++front];
-            System.out.print(curr.val + " ");
-            if (curr.left != null) que[++rear] = curr.left;
-            if (curr.right != null) que[++rear] = curr.right;
+    void levelOrder(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node curr = q.poll();
+            if (curr.left != null) q.add(curr.left);
+            if (curr.right != null) q.add(curr.right);
         }
         System.out.println();
     }
 
     // height
-    public int height(node root) {
+    int height(Node root) {
         if (root == null) return 0;
         int lt = height(root.left);
         int rt = height(root.right);
         return 1 + (lt > rt ? lt : rt);
     }
 
-    public int diameter(node root) {
+    int diameter(Node root) {
         if (root == null) return 0;
         int lt = height(root.left);
         int rt = height(root.right);
@@ -98,11 +103,12 @@ class tree {
 
 public class generalTree {
     public static void main(String[] args) {
-        tree T1 = new tree(1);
+        tree T1 = new tree();
+        T1.insert(T1.root, 1);
         T1.insert(T1.root, 2);
         T1.insert(T1.root, 3);
 
-        T1.inorder(T1.root);
+        T1.inOrder(T1.root);
         T1.levelOrder(T1.root);
 
         System.out.println("The height is " + T1.height(T1.root));
