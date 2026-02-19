af62beaf05de2172c83d957a88bbcff29c79a3f0
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-02-19 20:04:54 +0530

    a file added in array

---
+++ Array/reversing.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main () {
+    int arr[5] = {1, 0, 3, 7, 4};
+    int n = sizeof(arr) /sizeof(int);
+
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n-i-1] = temp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
