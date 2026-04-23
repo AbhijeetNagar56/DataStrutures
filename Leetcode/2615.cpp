635e8c88d63b09c8d352ecf1b3a97d5cdbabc469
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-04-23 22:09:26 +0530

    Leetcode 2615.cpp hashmap + prefix sum solution

--- Leetcode/2615.cpp
+++ Leetcode/2615.cpp
@@ -18,3 +18,70 @@ public:
 
     }
 };
+
+
+// Optimized Solution using Hash Map
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int, vector<int>> Map;
+
+        for (int i = 0; i < n; i++) {
+            Map[nums[i]].push_back(i);
+        }
+
+        vector<long long> arr(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (auto &idx : Map[nums[i]]) {
+                if(idx == i) continue;
+                arr[i] += abs(idx - i);
+            }
+        }
+
+        return arr;
+
+    }
+};
+
+// Optimized Solution using Hash Map and Prefix Sum
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int, vector<int>> Map;
+
+        for (int i = 0; i < n; i++) {
+            Map[nums[i]].push_back(i);
+        }
+
+        vector<long long> arr(n, 0);
+
+        for(auto &it : Map){
+            auto &pos = it.second;
+
+            long long sum = 0;
+            for(int x : pos) sum += x;
+
+            long long leftSum = 0;
+            int m = pos.size();
+
+            for(int i = 0; i < m; i++){
+                long long rightSum = sum - leftSum - pos[i];
+
+                long long left  = 1LL * pos[i] * i - leftSum;
+                long long right = rightSum - 1LL * pos[i] * (m-i-1);
+
+                arr[pos[i]] = left + right;
+
+                leftSum += pos[i];
+            }
+        }
+
+        return arr;
+
+    }
+};
