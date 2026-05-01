e4bd04e99b2fb9cf6977fb8c100c1202f101b785
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-01 17:33:23 +0530

    Leetcode 396.cpp optimal o(n)

--- Leetcode/396.cpp
+++ Leetcode/396.cpp
@@ -1,3 +1,4 @@
+// brute force, TLE
 class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
@@ -15,5 +16,31 @@ public:
         }
 
         return max_score;
+    }
+};
+
+
+// optimal solution, O(n)
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n = nums.size();
+
+        long sum = 0;
+        long F = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            F += (long)i * nums[i];
+        }
+
+        long ans = F;
+
+        for (int k = 1; k < n; k++) {
+            F = F + sum - (long)n * nums[n - k];
+            ans = max(ans, F);
+        }
+
+        return (int)ans;
     }
 };
