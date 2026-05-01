6ea4e9e9e253e6128b78a07bdec6024da0de643c
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-01 18:34:43 +0530

    Leetcode 111.cpp file

---
+++ Leetcode/111.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+
+        if(root->left == NULL) return 1 + right;
+        if(root->right == NULL) return 1 + left;
+        
+        return 1 + min(left, right);
+    }
+};
