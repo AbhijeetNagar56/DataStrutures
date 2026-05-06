b47f9a87e63db8cd66388bcd3e7e184b77e87405
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-06 07:34:28 +0530

    Leetcode 5.cpp file

---
+++ Leetcode/Apr_2026/5.cpp
@@ -0,0 +1,30 @@
+// expand around center approach
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        
+        int start = 0, maxLen = 1;
+        
+        auto expandAroundCenter = [&](int left, int right) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            return right - left - 1;
+        };
+        
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expandAroundCenter(i, i);      // Odd length palindromes
+            int len2 = expandAroundCenter(i, i + 1);  // Even length palindromes
+            int len = max(len1, len2);
+            
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        
+        return s.substr(start, maxLen);
+    }
+};
