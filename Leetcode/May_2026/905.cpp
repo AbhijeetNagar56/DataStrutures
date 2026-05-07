2faab80e2c9d2690ef5ba7ec1744b69eccd95cc4
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-08 00:53:00 +0530

    Leetcode 905.cpp file

---
+++ Leetcode/May_2026/905.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+
+        vector<int> res;
+
+        for(int num: nums) {
+            if(num % 2 == 0) res.push_back(num);
+        }
+        for(int num: nums) {
+            if(num % 2 == 1) res.push_back(num);
+        }
+        
+        return res;
+    }
+};
