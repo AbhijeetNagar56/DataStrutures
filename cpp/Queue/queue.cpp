874b5a9a88973d86e3eec3c8a528605baf2899a1
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-21 07:47:57 +0530

    stack and queue with array in cpp

--- cpp/Queue/queue.cpp
+++ cpp/Queue/queue.cpp
@@ -1,7 +1,73 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main () {
-    cout << "queue" << endl;
+// push back pop front isFull isEmpty front
+
+class que {
+private:
+    int* arr;
+    int front;
+    int rear;
+    int size;
+public:
+    
+    que(int s) {
+        arr = new int[s];
+        size = s;
+        front = -1;
+        rear = 0;
+    }
+    bool isFull() {
+        return rear == size;
+    }
+
+    bool isEmpty() {
+        return front == -1 || front == rear;
+    }
+
+    void push(int val) {
+        if(isFull()) {
+            cout << "Queue Overflow" << endl;
+            return;
+        }
+        if(front == -1) {
+            front = 0;
+        }
+        arr[rear++] = val;
+    }
+
+    int pop() {
+        if(isEmpty()) {
+            cout << "Queue Underflow" << endl;
+            return 0;
+        }
+        if(front == 0) {
+            front = rear = -1;
+        }
+        return arr[front++];
+    }
+
+    int peek_front() {
+        if(isEmpty()) {
+            cout << "Queue Underflow" << endl;
+            return 0;
+        }
+        return arr[front];
+    }
+};
+
+int main() {
+    que a(3);
+    
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.push(4);
+    
+    cout << "Top element: " << a.peek_front() << endl;
+    cout << "Popped element: " << a.pop() << endl;
+    cout << "Is full: " << a.isFull() << endl;
+    cout << "Is empty: " << a.isEmpty() << endl;
+    
     return 0;
 }
--- cpp/Stack/stack.cpp
+++ cpp/Stack/stack.cpp
@@ -1,8 +1,63 @@
 #include<iostream>
 using namespace std;
 
-int main () {
+class stack {
+private:
+    int* arr;
+    int top;
+    int size;
+public:
+    
+    stack(int s) {
+        arr = new int[s];
+        size = s;
+        top = -1;
+    }
+    bool isFull() {
+        return top == size - 1;
+    }
 
-    cout << "stck" << endl;
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    void push(int val) {
+        if(isFull()) {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        arr[++top] = val;
+    }
+
+    int pop() {
+        if(isEmpty()) {
+            cout << "Stack Underflow" << endl;
+            return 0;
+        }
+        return arr[top--];
+    }
+
+    int peek() {
+        if(isEmpty()) {
+            cout << "Stack Underflow" << endl;
+            return 0;
+        }
+        return arr[top];
+    }
+};
+
+int main() {
+    stack a(3);
+    
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.push(4);
+    
+    cout << "Top element: " << a.peek() << endl;
+    cout << "Popped element: " << a.pop() << endl;
+    cout << "Is full: " << a.isFull() << endl;
+    cout << "Is empty: " << a.isEmpty() << endl;
+    
     return 0;
 }
