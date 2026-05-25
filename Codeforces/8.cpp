e52d16f2fdabfe501ac956c2004fd9faca07c124
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-25 06:45:05 +0530

    Codeforces 977A

--- Codeforces/8.cpp
+++ Codeforces/8.cpp
@@ -1,27 +1,48 @@
 // 59A
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+// #include <cctype>
+// using namespace std;
+
+// int main () {
+// 	string s;
+// 	cin >> s;
+// 	int up = 0;
+// 	for(char c: s) {
+// 		if(c >= 'a' && c <= 'z') {
+// 			up++;
+// 		} else {
+// 			up--;
+// 		}
+// 	}
+// 	if(up < 0) {
+// 		transform(s.begin(), s.end(), s.begin(), ::toupper);
+// 		cout << s << endl;
+// 	} else {
+// 		transform(s.begin(), s.end(), s.begin(), ::tolower);
+// 		cout << s << endl;
+// 	}
+// 	return 0;
+// }
+
+
+// 977A
 #include <iostream>
-#include <string>
-#include <algorithm>
-#include <cctype>
 using namespace std;
 
-int main () {
-	string s;
-	cin >> s;
-	int up = 0;
-	for(char c: s) {
-		if(c >= 'a' && c <= 'z') {
-			up++;
+int main() {
+	int n, k;
+	cin >> n >> k;
+	while(k) {
+		if(n % 10 == 0) {
+			n /= 10;
 		} else {
-			up--;
+			n -= 1;
 		}
+		k--;
 	}
-	if(up < 0) {
-		transform(s.begin(), s.end(), s.begin(), ::toupper);
-		cout << s << endl;
-	} else {
-		transform(s.begin(), s.end(), s.begin(), ::tolower);
-		cout << s << endl;
-	}
+	cout << n << endl;
 	return 0;
 }
+
