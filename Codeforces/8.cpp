9ec97851e66fb17072e08f64067e6630d5163584
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-26 06:48:12 +0530

    Codeforces 69A

--- Codeforces/8.cpp
+++ Codeforces/8.cpp
@@ -28,21 +28,51 @@
 
 
 // 977A
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+// 	int n, k;
+// 	cin >> n >> k;
+// 	while(k) {
+// 		if(n % 10 == 0) {
+// 			n /= 10;
+// 		} else {
+// 			n -= 1;
+// 		}
+// 		k--;
+// 	}
+// 	cout << n << endl;
+// 	return 0;
+// }
+
+// 69A
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-	int n, k;
-	cin >> n >> k;
-	while(k) {
-		if(n % 10 == 0) {
-			n /= 10;
-		} else {
-			n -= 1;
-		}
-		k--;
+int main () {
+	int n;
+	cin >> n;
+	vector<vector<int>> forces(n, vector<int>(3, 0));
+	for(int i=0; i<n; i++) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		forces[i] = { x, y, z };
 	}
-	cout << n << endl;
+	vector<int> fnet(3,0);
+	for(vector<int> f: forces) {
+		fnet[0] += f[0];
+		fnet[1] += f[1];
+		fnet[2] += f[2];
+	}
+
+	if(fnet[0] == 0 && fnet[1] == 0 && fnet[2] == 0) {
+		cout << "YES" << endl;
+	} else {
+		cout << "NO" << endl;
+	}
+
 	return 0;
 }
 
