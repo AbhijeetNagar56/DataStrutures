826cef9b656286f4cdb53bbfd902c4120d58dc69
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-06-01 09:10:47 +0530

    codeforces 277A

--- Codeforces/9.cpp
+++ Codeforces/9.cpp
@@ -17,27 +17,49 @@
 // }
 
 // 271A
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+// bool isValid(int n) {
+//     int v[10] = {0};
+//     while(n) {
+//         int d = n % 10;
+//         if(v[d] != 0) return false;
+//         v[d] = 1;
+//         n/=10;
+//     }
+//     return true;
+// }
+// int main() {
+//     int n;
+//     cin >> n;
+//     for(int i=n+1; i<INT_MAX; i++) {
+//         if(isValid(i)) {
+//             cout << i << endl;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+
+// 677A
 #include <iostream>
-#include <climits>
 using namespace std;
-bool isValid(int n) {
-    int v[10] = {0};
-    while(n) {
-        int d = n % 10;
-        if(v[d] != 0) return false;
-        v[d] = 1;
-        n/=10;
-    }
-    return true;
-}
+
 int main() {
-    int n;
-    cin >> n;
-    for(int i=n+1; i<INT_MAX; i++) {
-        if(isValid(i)) {
-            cout << i << endl;
-            break;
-        }
+    int n, h;
+    cin >> n >> h;
+    int ans = 0;
+    for(int i=0; i<n; i++) {
+	    int a;
+	    cin >> a;
+	    if(a > h) {
+		    ans += 2;
+	    } else {
+		    ans++;
+	    }
     }
+    cout << ans << endl;
     return 0;
 }
