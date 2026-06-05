f1ae078e9012b356e558a3524edf68bca187f096
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-06-05 08:27:36 +0530

    codeforces 467A

--- Codeforces/9.cpp
+++ Codeforces/9.cpp
@@ -111,26 +111,44 @@
 // }
 
 // 266B
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+// using namespace std;
+// int main() {
+// 	int n, t;
+// 	cin >> n >> t;
+// 	string q;
+// 	q.resize(n);
+// 	for(int i=0; i<n; i++) {
+// 		cin >> q[i];
+// 	}
+// 	while(t--) {
+// 		for(int i=1; i<n; i++) {
+// 			if(q[i] == 'G' && q[i-1] == 'B') {
+// 				swap(q[i], q[i-1]);
+// 				i++;
+// 			}
+// 		}
+// 	}
+// 	cout << q << endl;
+// 	return 0;
+// }
+
+// 467A
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 int main() {
-	int n, t;
-	cin >> n >> t;
-	string q;
-	q.resize(n);
+	int n;
+	cin >> n;
+	int ans=0;
 	for(int i=0; i<n; i++) {
-		cin >> q[i];
-	}
-	while(t--) {
-		for(int i=1; i<n; i++) {
-			if(q[i] == 'G' && q[i-1] == 'B') {
-				swap(q[i], q[i-1]);
-				i++;
-			}
+		int p, q;
+		cin >> p >> q;
+		if(q-p>=2) {
+			ans++;
 		}
 	}
-	cout << q << endl;
+	cout << ans << endl;
 	return 0;
 }
