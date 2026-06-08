8335294a9076fc7acb407192b4527f8ad0435793
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-06-08 06:31:33 +0530

    codeforces 344A

--- Codeforces/7.cpp
+++ Codeforces/7.cpp
@@ -13,22 +13,48 @@
 
 
 // 118A
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+// 	string s;
+// 	cin >> s;
+//     string ans;
+//     for(int i = 0; i < s.size(); i++) {
+//         if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y' ||
+//             s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y') {
+//             continue;
+//         }
+//         ans += '.';
+//         ans += tolower(s[i]);
+//     }
+//     cout << ans << endl;
+// 	return 0;
+// }
+
+// 344A
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main() {
-	string s;
-	cin >> s;
-    string ans;
-    for(int i = 0; i < s.size(); i++) {
-        if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y' ||
-            s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y') {
-            continue;
+	int n; 
+	cin >> n;
+    int comp=0;
+    string pre = "";
+    while(n--) {
+        string m;
+        cin >> m;
+        if(pre=="") {
+            comp++;
+        } else {
+            if(pre[1] == m[0]) {
+                comp++;
+            }
         }
-        ans += '.';
-        ans += tolower(s[i]);
+        pre=m;
     }
-    cout << ans << endl;
+    cout << comp << endl;
+
 	return 0;
 }
