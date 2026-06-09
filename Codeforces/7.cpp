655d1e2bd7837fcb085e944df9824a9e36abf6a3
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-06-09 06:20:46 +0530

    codeforces 200B

--- Codeforces/7.cpp
+++ Codeforces/7.cpp
@@ -34,27 +34,46 @@
 // }
 
 // 344A
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main() {
+// 	int n; 
+// 	cin >> n;
+//     int comp=0;
+//     string pre = "";
+//     while(n--) {
+//         string m;
+//         cin >> m;
+//         if(pre=="") {
+//             comp++;
+//         } else {
+//             if(pre[1] == m[0]) {
+//                 comp++;
+//             }
+//         }
+//         pre=m;
+//     }
+//     cout << comp << endl;
+
+// 	return 0;
+// }
+
+// 200 B
 #include <iostream>
-#include <string>
+#include <iomanip>
 using namespace std;
 int main() {
-	int n; 
+	int n;
+    double sum=0; 
 	cin >> n;
-    int comp=0;
-    string pre = "";
+    int cpy=n;
     while(n--) {
-        string m;
-        cin >> m;
-        if(pre=="") {
-            comp++;
-        } else {
-            if(pre[1] == m[0]) {
-                comp++;
-            }
-        }
-        pre=m;
+        int p;
+        cin >> p;
+        sum+=p;
     }
-    cout << comp << endl;
-
+    double avg = sum / cpy;
+    cout << fixed << setprecision(12) << avg << endl;
 	return 0;
 }
