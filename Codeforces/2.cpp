ded7772201c8327d24885aa4ad4ddabf1e0752d2
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-06-20 10:51:00 +0530

    codeforces 148A

--- Codeforces/2.cpp
+++ Codeforces/2.cpp
@@ -24,24 +24,53 @@
 // }
 
 // 282A
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//     int n, total = 0;
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         string s;
+//         cin >> s;
+//         if(s == "++X" || s == "X++") {
+//             total++;
+//         } else {
+//             total--;
+//         }
+//     }
+
+//     cout << to_string(total) << endl;
+//     return 0;
+// }
+
+// 148A
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
-
 int main() {
-    int n, total = 0;
-    cin >> n;
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+    vector<bool> damage(d, false);
 
-    for(int i = 0; i < n; i++) {
-        string s;
-        cin >> s;
-        if(s == "++X" || s == "X++") {
-            total++;
-        } else {
-            total--;
-        }
+    for(int i=k-1; i<d; i+=k) {
+        damage[i]=true;
     }
-
-    cout << to_string(total) << endl;
+    for(int i=l-1; i<d; i+=l) {
+        damage[i]=true;
+    }
+    for(int i=m-1; i<d; i+=m) {
+        damage[i]=true;
+    }
+    for(int i=n-1; i<d; i+=n) {
+        damage[i]=true;
+    }
+    int ct = 0;
+    for(bool d: damage) {
+        if(d) ct++;
+    }
+    cout << ct << "\n";
     return 0;
 }
