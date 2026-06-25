679b0fb0fb824af523c4c8740600ef360bd8009b
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-06-25 19:00:47 +0530

    codeforces 1335A

--- Codeforces/2.cpp
+++ Codeforces/2.cpp
@@ -176,38 +176,63 @@
 // }
 
 //510A 
+// #include <iostream>
+// using namespace std;
+// void draw(int n, int m) {
+//     bool isRight = true;
+//     for(int i=0; i<n; i++) {
+//         if(i % 2 != 0) {
+//             if(!isRight) {
+//                 cout << "#";
+//             } else {
+//                 cout << ".";
+//             }
+//             for(int j=0; j<m-2; j++) {
+//                 cout << ".";
+//             }
+//             if(isRight) {
+//                 cout << "#";
+//             } else {
+//                 cout << ".";
+//             }
+
+//             isRight = !isRight;
+//         } else {
+//             for(int j=0; j<m; j++) {
+//                 cout << "#";
+//             }
+//         }
+//         cout << "\n";
+//     }
+// }
+// int main() {
+//     int n, m;
+//     cin >> n >> m;
+//     draw(n, m);
+//     return 0;
+// }
+
+// 1335A
 #include <iostream>
 using namespace std;
-void draw(int n, int m) {
-    bool isRight = true;
-    for(int i=0; i<n; i++) {
-        if(i % 2 != 0) {
-            if(!isRight) {
-                cout << "#";
-            } else {
-                cout << ".";
-            }
-            for(int j=0; j<m-2; j++) {
-                cout << ".";
-            }
-            if(isRight) {
-                cout << "#";
-            } else {
-                cout << ".";
-            }
-
-            isRight = !isRight;
-        } else {
-            for(int j=0; j<m; j++) {
-                cout << "#";
-            }
-        }
-        cout << "\n";
+long long perm(long long n) {
+    long long ans;
+    if(n % 2 == 0) {
+        ans = n / 2 - 1;
+    } else {
+        ans = n / 2;
     }
+
+    return ans;
 }
 int main() {
-    int n, m;
-    cin >> n >> m;
-    draw(n, m);
+    int t;
+    cin >> t;
+    while(t--) {
+        long long n;
+        cin >> n;
+        long long result = perm(n);
+        cout << result << "\n";
+    }
     return 0;
 }
