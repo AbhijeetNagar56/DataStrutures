14914642941bb99a9faf5ba602c9a410432c5761
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-07-08 19:22:20 +0530

    leetcode 43 code updation

--- Leetcode/July_2026/43.cpp
+++ Leetcode/July_2026/43.cpp
@@ -2,54 +2,35 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         if (num1 == "0" || num2 == "0") return "0";
+        int n1 = num1.size();
+        int n2 = num2.size();
 
-        if (num1.size() < num2.size()) {
-            return multiply(num2, num1);
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        vector<int> res(n1 + n2, 0);
+
+        for(int i=0; i<n1; i++) {
+            for(int j=0; j<n2; j++) {
+                int d1 = num1[i] - '0';
+                int d2 = num2[j] - '0';
+                int prod = d1*d2;
+                res[i+j] += prod;
+                res[i+j+1] += res[i+j] / 10;
+                res[i+j] = res[i+j] % 10;
+            }
         }
 
-        string res = "";
-        int zero = 0;
-        for (int i = num2.size() - 1; i >= 0; --i) {
-            string cur = mul(num1, num2[i], zero);
-            res = add(res, cur);
-            zero++;
+        int last = n1+n2-1;
+        while(last > 0 && res[last] == 0) {
+            last--;
         }
 
-        return res;
-    }
+        string ans = "";
 
-    string mul(string s, char d, int zero) {
-        int i = s.size() - 1, carry = 0;
-        int digit = d - '0';
-        string cur;
-
-        while (i >= 0 || carry) {
-            int n = (i >= 0) ? s[i] - '0' : 0;
-            int prod = n * digit + carry;
-            cur.push_back((prod % 10) + '0');
-            carry = prod / 10;
-            i--;
+        for(int i=last; i>=0; i--) {
+            ans += to_string(res[i]);
         }
-
-        reverse(cur.begin(), cur.end());
-        return cur + string(zero, '0');
-    }
-
-    string add(string num1, string num2) {
-        int i = num1.size() - 1, j = num2.size() - 1, carry = 0;
-        string res;
-
-        while (i >= 0 || j >= 0 || carry) {
-            int n1 = (i >= 0) ? num1[i] - '0' : 0;
-            int n2 = (j >= 0) ? num2[j] - '0' : 0;
-            int total = n1 + n2 + carry;
-            res.push_back((total % 10) + '0');
-            carry = total / 10;
-            i--;
-            j--;
-        }
-
-        reverse(res.begin(), res.end());
-        return res;
+        
+        return ans;
     }
 };
